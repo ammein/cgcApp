efc2ce2ec04bfb08fedf841f7eb544ccc693dcfd
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-24 18:29:21 +0800

    Done all front end and backend

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,101 @@
+function update(id, questionString , allAnswers) {
+    $.ajax({
+        method: "PATCH",
+        url: '/api/question/' + id,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            id,
+            questionString,
+            answers : allAnswers
+        }),
+        success: function (response) {
+            initialize();
+            // Empty the content to avoid duplicate content
+            $("#questionsBlock").empty();            
+        }
+    });
+}
+
+function deleteData(id, questionString) {
+    $.ajax({
+        method: "DELETE",
+        url: '/api/question/' + id,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            id
+        }),
+        success: function (response) {
+            initialize();
+            // Empty the content to avoid duplicate content
+            $("#questionsBlock").empty();
+        }
+    });
+}
+
+function initialize() {
+    return $.ajax({
+            method: "GET",
+            url: '/api/question',
+            contentType: 'application/json',
+            success: function (response) {
+                var getListBlock = $("#questionsBlock");
+                // console.log("All responses",{response});
+                response.question.forEach((questions) => {
+                    var id = questions._id;
+                    getListBlock.append("<form id='" + id + "' class='questionBox'><textarea class='list'>" + questions.questionString + "</textarea><input type='number' class='answer-box-edit' id='trueAnswerBox' value='" + questions.answers[0] + "'><input type='number' class='answer-box-edit' id='falseAnswerBox1' value='" + questions.answers[1] + "'><input type='number' class='answer-box-edit' id='falseAnswerBox2' value='" + questions.answers[2] + "'><input type='number' class='answer-box-edit' id='falseAnswerBox3' value='" + questions.answers[3] + "'><br><button form='" + id + "' class='delete'>Delete</button><button form='" + id + "' class='update'>Update</button></form><hr>");
+                });
+                $('.questionBox').on('click', '.update', function (e) {
+                    e.preventDefault();
+                    console.log("This Update",this);
+                    var id = $(this).attr('form');
+                    var questionString = $("form#" + id).find("textarea").val();
+                    var answer1 = $("form#" + id).find("#trueAnswerBox").val();
+                    var answer2 = $("form#" + id).find("#falseAnswerBox1").val();
+                    var answer3 = $("form#" + id).find("#falseAnswerBox2").val();
+                    var answer4 = $("form#" + id).find("#falseAnswerBox3").val();
+                    var allAnswers = [answer1 , answer2 , answer3 , answer4];
+                    update(id, questionString , allAnswers);
+                });
+                $('.questionBox').on('click', '.delete', function (e) {
+                    e.preventDefault();
+                    var id = $(this).attr('form');
+                    var questionString = $("form#"+id).find("textarea").val();
+                    deleteData(id, questionString);
+                });
+            }
+        });
+}
+
+function closeBox(){
+    $("#questionsBlock").empty();
+    $(".all-question").css('display' , 'none');
+    $(".sidebar").css({
+        width : "0%",
+        transitionDuration: "0.4s"
+    });
+    $(".overlay").css({
+        display : "none",
+        transitionDuration : "0.2s"
+    });
+}
+
+function openBox(){
+    $(".all-question").css('display', 'block');
+    $(".sidebar").css({
+        display : "block",
+        width: "55%",
+        transitionDuration: "0.4s"
+    });
+    $(".overlay").css({
+        display: "block",
+        transitionDuration: "0.2s"
+    });
+    initialize();
+}
+
+// DOMContentLoaded
 $(function(){
+
     console.log("I loaded in browser");
 
     $(".textarea-box").one("click" , function(){
@@ -16,49 +113,4 @@ $(function(){
         alert('Question : ' + question + "\n True Asnwer :" + trueAnswer + "\n False Answers : " + falseAnswer);
     });
 
-    function getId(response , num){
-        return console.log(response.question[num]._id);
-    }
-
-    $("#get").on("click",function(){
-        // GET Questions
-        $.ajax({
-            method: "GET",
-            url: '/api/question',
-            contentType: 'application/json',
-            success: function (response) {
-                var getListBlock = $("#questionsBlock");
-                // console.log("All responses",{response});
-                response.question.forEach((questions) => {
-                    var id = questions._id;
-                    getListBlock.append("<form id='" + id + "' class='questionBox'><textarea class='list'>" + questions.questionString + "</textarea><input type='number' class='answer-box-edit' id='trueAnswerBox' value='" + questions.answers[0] + "'><input type='number' class='answer-box-edit' id='falseAnswerBox' value='" + questions.answers[1] + "'><input type='number' class='answer-box-edit' id='falseAnswerBox' value='" + questions.answers[2] + "'><input type='number' class='answer-box-edit' id='falseAnswerBox' value='" + questions.answers[3] + "'><br><button form='"+id+"' class='delete'>Delete</button><button form='"+id+"' class='update' onclick='update()'>Update</button></form><hr>");
-                    // console.log("Id :", id);
-                });
-            }
-        });
-    });
-
-
-    function update(){
-        // DELETE Questions
-        $('form').on('click', '.update', function (e) {
-            e.preventDefault();
-            var id = $("form.questionBox").attr('id');
-            var questionString = $(".list").val();
-            console.log(id);
-            console.log("Update clicked");
-            // $.ajax({
-            //     method: "PUT",
-            //     url: '/api/question',
-            //     contentType: 'application/json',
-            //     data: JSON.stringify({
-            //         id,
-            //         questionString
-            //     }),
-            //     success: function (response) {
-            //         $("#get").click();
-            //     }
-            // });
-        });
-    }
 });
