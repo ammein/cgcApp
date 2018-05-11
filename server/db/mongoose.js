fc0c1c7ef523a9d6f67184a94614b62034e678b6
Amin Shazrin <aminshazrin@yahoo.com>
2018-05-12 01:44:35 +0800

    Change on mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ require('dotenv').config({
     silent: true
 });
 
-mongoose.connect(process.env.MONGO_HOST + "/QuestionApp" ||'mongodb://localhost:27017/QuestionApp');
+mongoose.connect(process.env.MONGO_HOST + "/api" ||'mongodb://localhost:27017/QuestionApp');
 
 module.exports = {
     mongoose
