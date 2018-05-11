25ce71884aaa251114973b8f3bc7c3e175af607e
Amin Shazrin <aminshazrin@yahoo.com>
2018-05-12 04:26:55 +0800

    Use this ip

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // process.env.MONGO_HOST + "/api"
 // 'mongodb://localhost:27017/QuestionApp'
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://210.195.147.148:27017/test');
 
 module.exports = {
     mongoose
